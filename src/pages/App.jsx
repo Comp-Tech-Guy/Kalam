0274fe661f77abec04dba24fcbb3ee609774880b
Comp-Tech-Guy <allthought123@gmail.com>
2026-03-04 23:00:38 -0800

    Just added some base elements needed in the home screen

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -2,7 +2,7 @@ import "./App.css"
 import { addData, editData, getData, removeData } from "../JS/fileSystem";
 import { useEffect, useState } from "react";
 
-function App(){
+function App() {
     const [data, setData] = useState(null);
     const dataRecieve = async () => {
         const json = await getData("userProfiles.json");
@@ -13,32 +13,19 @@ function App(){
         dataRecieve();
     }, [])
 
-    const addButton = () => {
-        const newData = {
-            "id": 1,
-            "wallaper": "222"
-        }
-        addData("userProfiles.json", newData)
-    }
-
-    const removeButton = () => {
-        removeData("userProfiles.json", 1)
-    }
-
-    const editButton = () => {
-        const data = {
-            "id": 1,
-            "wallaper": "333"
-        }
-        editData("userProfiles.json", data)
-    }
-
     return (
         <main className="AppPg">
             <h1>Welcome to Home</h1>
-            <button onClick={addButton}>Add</button>
-            <button onClick={removeButton}>Remove</button>
-            <button onClick={editButton}>Edit</button>
+            <button onClick={dataRecieve}>Refresh</button>
+            <ul>
+                {data ? (
+                    data.profiles.map((profile) => (
+                        <li key={profile.id}>{profile.Name}</li>
+                    ))
+                ) : (
+                    <li>Loading profiles.....</li>
+                )}
+            </ul>
         </main>
     );
 }
