4cd00220b3df07fa09b9b6cb19dd49de56d8f562
Comp-Tech-Guy <allthought123@gmail.com>
2026-05-31 08:56:31 -0700

    IDK what is the commit

--- src/pages/profile.jsx
+++ src/pages/profile.jsx
@@ -4,107 +4,137 @@ import { getLength, addData } from "../JS/fileSystem";
 import "./App.css";
 
 function ProfilePage() {
-    const [imagePath, setImagePath] = useState("");
-    const [rainLayout, setRainLayout] = useState("");
-    const [yasbYaml, setYasbYaml] = useState("");
-    const [yasbCSS, setYasbCSS] = useState("");
-    const [name, setName] = useState("");
-    const [added, setAdded] = useState(Boolean);
+  const [imagePath, setImagePath] = useState("");
+  const [rainLayout, setRainLayout] = useState("");
+  const [yasbYaml, setYasbYaml] = useState("");
+  const [yasbCSS, setYasbCSS] = useState("");
+  const [glazeWm, setGlazeWm] = useState("");
+  const [zebar, setZebar] = useState("");
+  const [name, setName] = useState("");
+  const [added, setAdded] = useState(Boolean);
 
-    const storeData = async () => {
-        const id = await getLength("userProfiles.json");
-        const data = {
-            "id": id,
-            "Name": name,
-            "RainmeterLayoutName": rainLayout,
-            "Wallaper-Path": imagePath,
-            "Yasb-Yaml": yasbYaml,
-            "Yasb-CSS": yasbCSS
-        }
-        setAdded(true)
-        await addData("userProfiles.json", data);
-        setAdded(false)
-        reset();
+  const storeData = async () => {
+    const id = await getLength("userProfiles.json");
+    const data = {
+      id: id,
+      Name: name,
+      RainmeterLayoutName: rainLayout,
+      "Wallaper-Path": imagePath,
+      "Yasb-Yaml": yasbYaml,
+      "Yasb-CSS": yasbCSS,
+      "GlazeWM-Config": glazeWm,
+    };
+    setAdded(true);
+    await addData("userProfiles.json", data);
+    setAdded(false);
+    reset();
+  };
+
+  const reset = () => {
+    setImagePath("");
+    setYasbCSS("");
+    setYasbYaml("");
+    setRainLayout("");
+    setGlazeWm("");
+    setName("");
+  };
+
+  async function selectFile() {
+    try {
+      const select = await open({
+        multiple: false,
+        directory: false,
+        title: "Select a file",
+        filters: [
+          {
+            name: "Images",
+            extensions: ["png", "jpg", "jpeg"],
+          },
+        ],
+      });
+
+      if (select === null) {
+        console.log("No file selected");
+        return;
+      }
+
+      setImagePath(select);
+    } catch (e) {
+      console.log("Error selecting file:", e);
+      return;
     }
+  }
 
-    const reset = () => {
-        setImagePath("");
-        setYasbCSS("");
-        setYasbYaml("");
-        setRainLayout("");
-        setName("");
-    }
-
-    async function selectFile() {
-        try {
-            const select = await open({
-                multiple: false,
-                directory: false,
-                title: "Select a file",
-                filters: [{
-                    name: "Images",
-                    extensions: ["png", "jpg", "jpeg"]
-                }]
-            });
-
-            if (select === null) {
-                console.log("No file selected");
-                return;
-            }
-
-            setImagePath(select);
-        } catch (e) {
-            console.log("Error selecting file:", e);
-            return;
-        }
-    }
-
-    return (
-        <div className="profilePg">
-            <h1>Add Profile</h1>
-            <div className="profileCont">
-                <div>
-                    Name:
-                    <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
-                </div>
-                <div>
-                    Wallpaper-IMG:
-                    {imagePath ? (
-                        <button onClick={selectFile}>Selected</button>
-                    ) : (
-                        <button onClick={selectFile}>Select file</button>
-                    )}
-                </div>
-                <div>
-                    Rainmeter-Layout-Name:
-                    <input type="text" value={rainLayout} onChange={(e) => setRainLayout(e.target.value)} />
-                </div>
-                <div>
-                    Yasb-Yaml-Code:
-                    <textarea
-                        value={yasbYaml}
-                        rows={9}
-                        onChange={(e) => setYasbYaml(e.target.value)}
-                    />
-                </div>
-                <div>
-                    Yasb-CSS-Code:
-                    <textarea
-                        value={yasbCSS}
-                        rows={9}
-                        onChange={(e) => setYasbCSS(e.target.value)}
-                    />
-                </div>
-                <div>
-                    {added ? (
-                        <button>Added</button>
-                    ) : (
-                        <button onClick={storeData}>Add Profile</button>
-                    )}
-                </div>
-            </div>
+  return (
+    <div className="profilePg">
+      <h1>Add Profile</h1>
+      <div className="profileCont">
+        <div>
+          Name:
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
         </div>
-    )
+        <div>
+          Wallpaper-IMG:
+          {imagePath ? (
+            <button onClick={selectFile}>Selected</button>
+          ) : (
+            <button onClick={selectFile}>Select file</button>
+          )}
+        </div>
+        <div>
+          Rainmeter-Layout-Name:
+          <input
+            type="text"
+            value={rainLayout}
+            onChange={(e) => setRainLayout(e.target.value)}
+          />
+        </div>
+        <div>
+          Yasb-Yaml-Code:
+          <textarea
+            value={yasbYaml}
+            rows={9}
+            onChange={(e) => setYasbYaml(e.target.value)}
+          />
+        </div>
+        <div>
+          Yasb-CSS-Code:
+          <textarea
+            value={yasbCSS}
+            rows={9}
+            onChange={(e) => setYasbCSS(e.target.value)}
+          />
+        </div>
+        <div>
+          GlazeWM-Config:
+          <textarea
+            value={glazeWm}
+            rows={9}
+            onChange={(e) => setGlazeWm(e.target.value)}
+          />
+        </div>
+        <div>
+          Zebar-Config:
+          <textarea
+            value={zebar}
+            rows={9}
+            onChange={(e) => setZebar(e.target.value)}
+          />
+        </div>
+        <div>
+          {added ? (
+            <button>Added</button>
+          ) : (
+            <button onClick={storeData}>Add Profile</button>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default ProfilePage
+export default ProfilePage;
